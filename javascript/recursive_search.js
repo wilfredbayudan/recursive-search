eef6ed5ff5a9581c7a8175719b21dfc25ec0e5c8
Wilfred <wilfredbayudan@gmail.com>
2021-10-11 20:38:38 -1000

    commit tree a52174566e987a92cbfebce7c1e4b28566bddafb

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,13 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  if (arr.length === 0) {
+    return false;
+  }
+
+  if (arr[0] === target) {
+    return true;
+  }
+  return recursiveSearch(arr.slice(1), target);
+  
 }
 
 if (require.main === module) {
@@ -17,3 +25,27 @@ module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+Use recurssion to find if a target value is in an array of values. Return true if target is found, return false if not.
+
+Examples
+
+[1,2,3,4] and target of 5, return false
+[1,2,3,4] and target of 4, return true
+
+function, take an array (arr) and target (target)
+
+  if array length is 0
+    return false
+
+  // continue
+  if array at position 0 is equal to target
+    return true
+  else
+    recursive call to function with array minus value at 0 index
+
+
+
+
+*/
